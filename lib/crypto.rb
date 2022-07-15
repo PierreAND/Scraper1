4aa9db2facde5ee527f27c8005a8a3c78c4f09a7
PierreAND <andrieu@gmail.com>
2022-07-15 15:31:07 +0200

    nex1

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -4,20 +4,32 @@ require 'pry'
 require 'open-uri'
 
 
-def scrap_symbols
-  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-  20.times do |i| 
-    all_cryptos_symbols = page.xpath("//*[@id='__next']/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]/div/a[1]").text
-    puts all_cryptos_symbols
-  end
-  
+def url 
+  page = doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
 end
 
-def scrap_prices
-  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-  20.times do |i|
-    all_cryptos_prices = page.xpath("//*[@id='__next']/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[5]").text
-  puts all_cryptos_prices
+def noms_crypto
+  page = url
+  cryptos = []
+  page.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody //div/a[contains(@class, "cmc-table__column-name--symbol")]').each do |x|
+    cryptos.push(x.text)
   end
+  return cryptos
 end
- 
+
+def prix_crypto
+  page = url 
+  valeur = []
+  page.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody //tr/td[contains(@class, "cmc-table__cell--sort-by__price")]').each do |node|
+	valeur.push(node.text[1..-1].gsub(/[^\d^.]/,'').to_f)
+  end
+  return valeur
+end
+
+def array_crypto
+  cryptos = noms_crypto
+  valeur = prix_crypto
+  final_array = cryptos.zip(valeur).map{|k, v| {k =>v}}
+  return final_array
+end
+puts array_crypto
